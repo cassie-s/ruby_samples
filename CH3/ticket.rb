50eac2ca489c1038dd591388cb4633ccb2d30bdd
Cassie Simpson <cassie.s.simpson@gmail.com>
2023-06-09 13:44:37 -0500

    more samples

--- CH3/ticket.rb
+++ CH3/ticket.rb
@@ -1,18 +1,38 @@
 class Ticket
-    def initialize(venue,date)
+    def initialize(venue)
         @venue = venue
-        @date = date
     end
     def venue
         @venue
     end
+    def date=(date)
+        year, month, day = date.split("-")
+        if year.length == 4 && month.length == 2 && day.length == 2
+            @date = date
+        else
+            puts "Please submit the date in the format 'yyy-mm-dd'"
+        end
+    end
     def date
         @date
     end
+    def price=(amount)
+        if (amount * 100).to_i == amount * 100
+            @price = amount
+        else
+            puts "The price seems to be malformed"
+        end
+    end
+    def price
+        @price
+    end
+    def discount(percent)
+        @price = @price * (100 - percent) / 100.0
+    end
 end
 
-th = Ticket.new("Town Hall", "2013-11-12")
-cc = Ticket.new("Convention Center", "2014-12-13")
-puts "We've created two tickets."
-puts "The first is for a #{th.venue} event on #{th.date}."
-puts "The second is for an event on #{cc.date} at #{cc.venue}."
+ticket = Ticket.new("Town Hall")
+ticket.date = "1913-11-12"
+ticket.price=100.00
+puts "The ticket is for an event on #{ticket.date} at #{ticket.venue}."
+puts "The ticket for #{ticket.venue} has been discounted 15% to #{ticket.discount(15)}."
